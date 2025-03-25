52c1cbf7d0db78f771ac9de7aa024ba6af2eb3c9
FelipeDNL <felipednlled@gmail.com>
2025-03-25 14:03:11 -0300

    movendo para estilização de componentes

--- src/app/features/livros/adicionar-livro/adicionar-livro.component.ts
+++ src/app/features/livros/adicionar-livro/adicionar-livro.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { LivroService } from '../../../core/services/livro.service';
+import { Livro } from '../../../core/models/livro.model';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-adicionar-livro',
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './adicionar-livro.component.html',
-  styleUrl: './adicionar-livro.component.scss'
+  styleUrls: ['./adicionar-livro.component.scss']
 })
 export class AdicionarLivroComponent {
+  livro: Livro = { id: 0, titulo: '', autor: '', editora: '', ano: 0 };
 
+  constructor(private livroService: LivroService, private router: Router) {}
+
+  adicionarLivro(): void {
+    this.livro.id = Math.max(...this.livroService.getLivros().map(l => l.id)) + 1;
+    this.livroService.addLivro(this.livro);
+    this.router.navigate(['/livros']);
+  }
 }
--- src/app/features/livros/lista-livros/lista-livros.component.ts
+++ src/app/features/livros/lista-livros/lista-livros.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { LivroService } from '../../../core/services/livro.service';
+import { Livro } from '../../../core/models/livro.model';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-lista-livros',
-  imports: [],
+  imports: [ CommonModule, RouterLink ],
   templateUrl: './lista-livros.component.html',
   styleUrl: './lista-livros.component.scss'
 })
 export class ListaLivrosComponent {
+  livros: Livro[] = [];
 
+  constructor(private livroService: LivroService) {
+    this.livros = this.livroService.getLivros();
+  }
+
+  deleteLivro(id: number): void {
+    if (confirm('Tem certeza que deseja excluir este livro?')) {
+      this.livros = this.livros.filter(livro => livro.id !== id);
+    }
+  }
 }
