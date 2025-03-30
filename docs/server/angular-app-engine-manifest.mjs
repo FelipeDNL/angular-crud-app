41b33a76b5c08f4c6dfbd7e179449027f20e9358
FelipeDNL <felipednlled@gmail.com>
2025-03-30 18:29:17 -0300

    deploy

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/angular-crud-app',
+  basePath: 'https://felipednl.github.io/angular-crud-app',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/angular-crud-app',
+  baseHref: 'https://felipednl.github.io/angular-crud-app/',
   locale: undefined,
   routes: [
   {
@@ -32,13 +32,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 872, hash: 'e2e9a8a97464b4051613db4a4bb28870661843782990496e12fd4f9d09bf41e1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1028, hash: '7412077737609eb6460f841696e8c51d519b8d0fc62dc548a878cfa24dd80e68', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 9738, hash: 'f7bc024abf7263a211c9ccd3757e0bfaed5c22b49f9b447ae2b91742620420ef', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'livros/editar/1/index.html': {size: 5938, hash: 'a7d5aac034f2f8bbf0d4e5e641a4c4cc374e1938dcf329588bc5a5a546ac1317', text: () => import('./assets-chunks/livros_editar_1_index_html.mjs').then(m => m.default)},
-    'livros/adicionar/index.html': {size: 5926, hash: '3bdeec156f7c7c931099e71f3c1ac4ab16856559330a8dbb2f057419bc3ebc39', text: () => import('./assets-chunks/livros_adicionar_index_html.mjs').then(m => m.default)},
-    'livros/editar/2/index.html': {size: 5936, hash: 'e4bfa9ee6fd327300177c2d093fa165e66d403e290ee9759da8641cef5fe285d', text: () => import('./assets-chunks/livros_editar_2_index_html.mjs').then(m => m.default)},
-    'livros/editar/3/index.html': {size: 5936, hash: '6fbc3b195895585d6942c27fb0c50b7365de34d7230e724df13d1dbe1d4aeb92', text: () => import('./assets-chunks/livros_editar_3_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 900, hash: '03626fe99007a40e89781d2e170d26087e58f05e1222e3bdbf6c987833692c2c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1056, hash: '8ff000d6a55f48c2a1ee801538901bd05ae40364b37bc5e8cfd3ef3e2bcd97b8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'livros/editar/1/index.html': {size: 5966, hash: 'f8ae29c7afd218166e995b51685ffad5fe95563699d5740fe05434f7f6225a56', text: () => import('./assets-chunks/livros_editar_1_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 9874, hash: '920be43036e8b0b1a3f634efac5af142763eb2d49d7efbf6040c10c0ca6d2533', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'livros/adicionar/index.html': {size: 5954, hash: '41017128547d9eaa9d105d4d1eff60293095e55ff985375c42440625d0b669ab', text: () => import('./assets-chunks/livros_adicionar_index_html.mjs').then(m => m.default)},
+    'livros/editar/2/index.html': {size: 5964, hash: '02c35e8afd8351369e2506c8225e5a1964dc5321470b1fe2c3104feec3ba3ecb', text: () => import('./assets-chunks/livros_editar_2_index_html.mjs').then(m => m.default)},
+    'livros/editar/3/index.html': {size: 5964, hash: '9f9c960a01e68deac98afba08ae4c10fce0b4644c48d89e622cb71a6992d0eef', text: () => import('./assets-chunks/livros_editar_3_index_html.mjs').then(m => m.default)},
     'styles-GEHPKYSX.css': {size: 322, hash: 'eWB2s6qVfl8', text: () => import('./assets-chunks/styles-GEHPKYSX_css.mjs').then(m => m.default)}
   },
 };
